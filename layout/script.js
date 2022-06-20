0e951ad48af09d738cfa5ac1742f2c1b2866391a
diogoo <diogogpontes@gmail.com>
2022-06-20 12:29:58 -0300

    versão 4

--- layout/script.js
+++ layout/script.js
@@ -1,57 +1,57 @@
-function calcularGorjeta(event){
-    event.preventDefault();
-    let bill = document.getElementById('bill').value;
-    let serviceQual = document.getElementById('serviceQual').value;
-    let numOfPeople = document.getElementById('people').value;
-   
+//function calcularGorjeta(event){
+//event.preventDefault();
+  //  let bill = document.getElementById('bill').value;
+  //  let serviceQual = document.getElementById('serviceQual').value;
+   // let numOfPeople = document.getElementById('people').value;
+   //
+   // if(bill == '' | serviceQual == 0 ){
 
-    if(bill == '' | serviceQual == 0 ){
-        alert("Por favor preencha os campos")
-        return;
-    }
+   // alert("Por favor preencha os campos")
+    //    return;
+   // }
+   // if(numOfPeople =="" | numOfPeople <= 1){
 
-    if(numOfPeople =="" | numOfPeople <= 1){
-        numOfPeople = 1;
-        document.getElementById('each').style.display = "none"
-    } else{
-        document.getElementById('each').style.display = "block"
-    }
-
-    document.getElementById('container').style.display = "block";
-} 
-function confirmacaoLogin(event){
-    event.preventDefault();
-    let perguntaConfirmacao = document.getElementById('confirmacao').value
-    let cadastrarUser = document.getElementById('cadastroForm').value
-
-    if(perguntaConfirmacao == '' ){
-        alert("Por favor preencha o campo")
-        return;
-    } if(perguntaConfirmacao == 'nao'){
-        perguntaConfirmacao = 'nao';
-        document.getElementById('cadastrarUser').style.display = "none"
-    } else {
-        document.getElementById('sim').style.display = "block";
-    }
-
-    document.getElementById('container').style.display = "block";   
-
-    
-}
-
-
-function cadastrarUser (event){
-    event.preventDefault();
-    let logConfirm = document.getElementById('confirmacao').value;
-
-    if(logConfirm == "nao"){
-        document.getElementById('container').style.display = "none";
-        document.getElementById('each').style.display = "none";
-    } else{
+    //numOfPeople = 1;
+      //  document.getElementById('each').style.display = "none"
+   // } else{
+        //document.getElementById('each').style.display = "block"
+   // }
+//
+  //  document.getElementById('container').style.display = "block";
+//} 
+//function confirmacaoLogin(event){
+    //event.preventDefault();
+    //let perguntaConfirmacao = document.getElementById('confirmacao').value
+   // let cadastrarUser = document.getElementById('cadastroForm').value
+//
+   // if(perguntaConfirmacao == '' ){
+   //     alert("Por favor preencha o campo")
+   //     return;
+   // } if(perguntaConfirmacao == 'nao'){
+    //    perguntaConfirmacao = 'nao';
+   //  document.getElementById('cadastrarUser').style.display = "none"
+  //  } else {
+   //     document.getElementById('sim').style.display = "block";
+   // }
+//
+   // document.getElementById('container').style.display = "block";   
+//
+   // 
+//}
+//
+//
+//function cadastrarUser (event){
+  // event.preventDefault();
+  // let logConfirm = document.getElementById('confirmacao').value;
+//
+ //if(logConfirm == "nao"){
+//       document.getElementById('container').style.display = "none";
+ //      document.getElementById('each').style.display = "none";
+//    } else{
         
-    }
-
-}
-
-
-document.getElementById('tipsForm').addEventListener('submit' ,calcularGorjeta);
+  //  }
+//
+//}
+//
+//
+//document.getElementById('tipsForm').addEventListener('submit' ,calcularGorjeta);
